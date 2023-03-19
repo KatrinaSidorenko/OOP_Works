27166737ca6590d6ef5959d4ba1f36d9557a163c
Katrina Sidorenko <ekaterinasidorenko0914@gmail.com>
2023-03-19 11:56:36 +0200

    Homework1

---
+++ Homework1_BomberMan/Homework1_BomberMan/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_BomberMan
+{
+    public class Constant
+    {
+        public const char PlayerChar = 'I';
+        public const char BombChar = '@';
+        public const char ConcreteWallChar = '#';
+        public const char TempWallChar = '&';
+        public const char EmptySpaceChar = ' ';
+    }
+}
---
+++ Homework1_BomberMan/Homework1_BomberMan/Map.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_BomberMan
+{
+    public class Map
+    {
+        private  GameObject[,] gameObjectsMap;
+        private int _xSize = 16;
+        private int _ySize = 16;
+
+        public Map()
+        {
+            gameObjectsMap = new GameObject[_xSize, _ySize];
+        }
+
+    }
+}
